aad345d149c673f9a68811c90b79029fcfd0dc55
dmike16 <cipmiky@gmail.com>
2015-04-03 04:56:01 +0200

    Done: add the history block + add th facebook and gplus button  began to write location html section. toDo: insert the hour of work + drow an image for location

--- www/public_html/min-instance.js
+++ www/public_html/min-instance.js
@@ -1,6 +1,10 @@
-function inserMainScript(url,callback){
+function inserMainScript(url,callback,sID){
     var script = document.createElement("script");
-    script.async =" ";script.type="text/javascript";
+    script.async=true;script.type="text/javascript";
+    if(sID){
+        script.id=sID;
+    }
+    
     if(script.readyState){
         script.onreadystatechange = function(){
             if(script.readyState == "loaded"||script.readyState =="complete")
@@ -19,6 +23,24 @@ function inserMainScript(url,callback){
         script=null;
 }
 
-inserMainScript("main.js",function(){
+/*inserMainScript("main.js",function(){
     alert("scipt loaded");
+});*/
+(function(){
+    window.___gcfg={
+        lang:'it',
+        parsetags:'explicit'
+    };
+    inserMainScript("https://apis.google.com/js/plusone.js",function(){
+    gapi.plusone.render("pOne-16",{"size":"medium","href":"https://plus.google.com/110996179998640315374/posts/gMbggKTMxyZ"});
 });
+    fbInit=function(){
+        FB.init({
+                appId   :'1404574083193147',
+                xfbml   :true,
+                version :'v2.3'
+        });
+    };
+    inserMainScript("http://connect.facebook.net/it_IT/sdk.js",fbInit,"facebook-jssdk");
+})();
+
