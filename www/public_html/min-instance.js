d19bb48da5cf1ea2e193c6d9b9ae1c97a28d6f32
dmike16 <cipmiky@gmail.com>
2015-06-18 19:52:05 +0200

    Change footer + add the paper button like the polymer ones

--- www/public_html/min-instance.js
+++ www/public_html/min-instance.js
@@ -47,7 +47,6 @@ function inserMainScript(url, callback, sID) {
     inserMainScript("https://connect.facebook.net/it_IT/sdk.js", fbInit, "facebook-jssdk");
     
     //Insert the twitter share button
-    inserMainScript("https://platform.twitter.com/widgets.js", function () {return; }, "twitter-wjs");
     inserMainScript("interaction.js", function(){return;});
 
 }());
