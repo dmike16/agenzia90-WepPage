ebdb29d2d45c4f2d8276155be2dac7893daacf33
dmike16 <cipmiky@gmail.com>
2015-07-30 18:43:34 +0200

    Improve the logical Structure
    Add some object and general function to improve the logical. Need to
    rewrite in the same form also the over and out event of the location
    section.

--- WebComponent/www/public_html/script/interaction.js
+++ WebComponent/www/public_html/script/interaction.js
@@ -5,14 +5,46 @@
         function onscroll_() {
             this.previewScroll = this.lastScroll;
             this.lastScroll = window.scrollY || window.pageYOffset;
-            requestTick.bind(this)();
+            this._requestTick();
         }
-        function requestTick() {
+        function onresize_(){
+        	this.domElement.innerHeight = obj.global.innerHeight;
+        	this._requestTick();
+        }
+        function requestTick_() {
                 if (!this.ticking) {
-                    obj.animatedFrame.request(this.update.bind(this));
+                    obj.animatedFrame.request(this._update);
                     this.ticking = true;
                 }
         }
+        function update_(){
+        	this.update();
+        	this.ticking = false;
+        }
+        function _setActive(arr) {
+        	var len = arr.length;
+        	
+        	for(var i = 0; i < len; i++) {
+        		crossClassList(arr[i]).add("active");
+        	}
+        }
+        function animateOnHover(e){
+        	var ele = middleTarget;
+        	var eType = e.type;
+        	var mouseOver;
+        	
+        	switch(eType){
+        	case 'mouseover':
+        		mouseOver = true;
+        		break;
+        	case 'mouseout':
+        		mouseOver = false;
+        		break;
+        	default:
+        		return;
+        	}
+        	crossClassList(ele).toggle('animate',mouseOver);
+        };
         //
         // Dependencies
         var cssSelector = obj.doc.querySelector.bind(obj.doc) 
@@ -26,25 +58,24 @@
         ,id_p = 0
         //
         // Dom Element
-        ,pan1 = cssSelector("#pannel3")
-        ,pan2 = cssSelector("#pannel4")
         ,sch = cssSelector(".header .search")
-        ,fa_cog_icon = cssSelectorAll("i.fa-cog")
         ,mask_modal = cssSelector("div.mask-modal")
         ,jb = cssSelector("button.paper-fab")
         ,arrowHint = cssSelector(".section-intro .arrow-hint")
         ,headLogo = cssSelector(".header .logo")
         ,photoCrop = cssSelector(".section-base .photo-crop")
+        ,bus = cssSelector("#location .bus p")
+        ,car = cssSelector("#location .car p")
+        ,middleTarget = cssSelector("#location .middle-target")
         //Variables
         //
         ,visibility = 'hidden'
-        ,o_date = null
-        ,appBar = {}
         ,scrollingPaper = {
             view: obj.global,
             body: obj.doc.body,
             lastScroll: 0,
             previewScroll: 0,
+            _allActive: false,
             firstScroll: false,
             ticking: false,
             update: function () {
@@ -65,11 +96,13 @@
                     if (lscroll >= thh && !this.tirth) {
                         var qhh = -ihh/5;
                         section.style.transform="translate3d(0px," + qhh +"px,0px)";
+                        section.style.zIndex = "-1";
                         content.style.opacity="0.01";
                         headLogo.style.opacity = "1.0";
                         this.tirth = true;
                     } else if (lscroll < thh && this.tirth) {
                         section.style.transform="translate3d(0px, 0px,0px)";
+                        section.style.zIndex="0";
                         content.style.opacity="1.0";
                         headLogo.style.opacity = "0.0";
                         this.tirth = false;
@@ -80,8 +113,27 @@
                             this.body.dispatchEvent(new Event('scrolledIntroPage'));
                         }
                     }
+                } else if (!this._allActive) {
+                	var nih,lim;
+                	if(!this._catalogActive){
+                		nih = 2*ihh
+                    	lim = nih*0.8;
+                		if (lscroll > lim) {
+                			this._catalogActive = true;
+                			_setActive(this.catalogServices);
+                			this.catalogServices = null;
+                		}
+                	} else if (!this._locationActive){
+                		nih = 3*ihh;
+                		lim = nih*0.8;
+                		if (lscroll > lim) {
+                			this._locationActive = true;
+                			this._allActive = true;
+                			_setActive(this.location);
+                			this.location = null;
+                		}
+                	}
                 }
-                this.ticking = false;
             },
             onOffPaper: function () {
                 var fs = this.firstScroll;
@@ -91,6 +143,8 @@
             },
             tirth: false,
             init: function () {
+            	this._update = update_.bind(this);
+            	this._requestTick = requestTick_.bind(this);
                 this.scroll = onscroll_.bind(this);
                 this.scroll();
                 
@@ -106,16 +160,6 @@
                         },
             ticking: false,
             update: function () {
-                this.fit();
-                this.ticking = false;    
-            },
-            requestTick: function () {
-                if (!this.ticking) {
-                    obj.animatedFrame.request(this.update.bind(this));
-                    this.ticking = true;
-                }
-            },
-            fit: function () {
                 var domEle = this.domElement
                 ,hh = domEle.innerHeight
                 ,shh = hh + "px"
@@ -135,12 +179,12 @@
                 }
             },
             init: function () {
-                var _that = this;
-                _that.fit();
-                aEventListener(obj.global, "resize", function (){
-                    _that.domElement.innerHeight = obj.global.innerHeight;
-                    _that.requestTick.call(_that,null);
-                });
+            	this._update = update_.bind(this);
+            	this._requestTick = requestTick_.bind(this);
+                this.resize = onresize_.bind(this);
+                this.resize();
+                
+                aEventListener(obj.global, "resize", this.resize);
                 delete(this.init);
             }
         };
@@ -149,75 +193,11 @@
             fromObj : makeFullScreen.domElement,
             contentOpacity : cssSelector(".section-intro .primary-block")
         };
+        scrollingPaper.catalogServices = cssSelectorAll("#catalog .service-title-0");
+        scrollingPaper._catalogActive = false;
+        scrollingPaper.location = cssSelectorAll("#location .map");
+        scrollingPaper._locationActive = false;
 
-        // Structure app bar
-        appBar.main = cssSelector(".bar");
-        appBar.search = sch;
-        appBar.id = 0;
-        appBar.pannel_on = false;
-        appBar.search_on = false;
-        appBar.pannel_active = "";
-        appBar.pannel3 = pan1;
-        appBar.pannel4 = pan2;
-
-        appBar.pannel3.scroller = {};
-        appBar.pannel3.scroller.ele = pan1.querySelector(".scroller");
-        appBar.pannel3.ripple = pan1.querySelector(".ripple");
-        appBar.pannel3.background = pan1.querySelector(".background");
-        appBar.pannel3.scroller.css_style = "width:193px;height:140px;max-width:1459px;max-height:443px;";
-
-        appBar.pannel4.scroller = {};
-        appBar.pannel4.scroller.ele = pan2.querySelector(".scroller");
-        appBar.pannel4.scroller.css_style = "width:160px;height:135px;max-width:1459px;max-height:443px;";
-        appBar.pannel4.ripple = pan2.querySelector(".ripple");
-        appBar.pannel4.background = pan2.querySelector(".background");
-
-        
-        appBar.openPannel = function openPannel(str) {
-            var scroller = appBar[str].scroller
-            ,ripple = appBar[str].ripple
-            ,background = appBar[str].background;
-
-            appBar.pannel_on = true;
-            appBar.pannel_active = str;
-            scroller.ele.setAttribute("style",scroller.css_style);
-            appBar[str].setAttribute("style","outline:none;z-index:12;right:0px;top:0px;");
-            obj.animatedFrame.cancel(id_p);
-            id_p = obj.animatedFrame.request(function(){
-                crossClassList(appBar[appBar.pannel_active]).add("active");
-            });
-        };
-        appBar.closePannel = function closePannel(str) {
-            var scroller = appBar[str].scroller;
-
-            appBar.pannel_on = false;
-            appBar.pannel_active = " ";
-            appBar[str].setAttribute("style","outline:none;display:none;");
-            scroller.ele.removeAttribute("style");
-            crossClassList(appBar[str]).remove("active");
-        };
-        appBar.handlerEvent = function handlerEvent(str) {
-            if (appBar.pannel_on) {
-                appBar.closePannel(appBar.pannel_active);
-                if (!appBar.pannel_active.match(str)) {
-                    appBar.openPannel(str);
-                }
-            } else {
-                    appBar.openPannel(str);
-            }
-        };
-        appBar.showSearch = function showSearch() {
-            if(!appBar.search_on) {
-                crossClassList(appBar.main).add('search-on');
-                crossClassList(appBar.search).add('active');
-                appBar.search_on = true;
-            }
-        };
-        appBar.closeSearch = function closeSearch() {
-                crossClassList(appBar.main).remove('search-on');
-                crossClassList(appBar.search).remove('active');
-                appBar.search_on = false;
-        };
         //
         // Event Callback function
         //
@@ -250,7 +230,6 @@
         // Add Event Listenrs
         aEventListener(jb, 'click', gestureEvent);
         aEventListener(mask_modal, 'click', gestureEvent);
-//        aEventListener(obj.global, "scroll", gestureEvent);
         aEventListener(obj.doc.body, 'scrolledIntroPage', function (e){
                 aEventListener(photoCrop,'transitionend',function dmp(e){
                     var paperDinamyc = e.currentTarget.querySelector(".paper-fab[dynamic]");
@@ -261,78 +240,10 @@
                 photoCrop = null;
             e.stopPropagation();
         });
-        //Event Gesture app-bar
-        //-*- Open pannel in main nav bar
-        //-*- Open serch form in app bar
-        aEventListener(appBar.pannel3.parentElement,'mouseup',function(e) {
-            var tmp_id = appBar.id;    
-            obj.animatedFrame.cancel(tmp_id);
-            function wrappH () {
-                appBar.handlerEvent('pannel3');
-            }
-            tmp_id = obj.animatedFrame.request(wrappH);
-            e.currentTarget.removeAttribute("pressed");
-            e.currentTarget.removeAttribute('active');
-/*if(target.getAttribute('aria-label') === 'search') {
-                if(appBar.pannel_on) {
-                    appBar.closePannel(appBar.pannel_active);
-                }
-                appBar.showSearch();
-                e.preventDefault();
-                e.stopPropagation();
-            } else if(appBar.search_on) {
-                if(target.nodeName !== 'FORM') {
-                    appBar.closeSearch();
-                }
-                e.stopPropagation();
-            } else if(appBar.pannel_on) {
-                appBar.closePannel(appBar.pannel_active);
-                e.stopPropagation();
-            }  else {
-                return;
-            }*/
-        });
-        aEventListener(appBar.pannel3.parentElement,'mousedown',function(e) {
-            e.currentTarget.setAttribute('pressed','');
-            e.currentTarget.setAttribute('active','');
-        });
-        aEventListener(appBar.pannel3,'mouseup',function(e){
-            e.stopPropagation();
-        });
-        aEventListener(obj.doc,'click', function(e) {
-            if(appBar.pannel_on) { 
-                appBar.closePannel(appBar.pannel_active);
-            } else if (appBar.search_on) {
-                appBar.closeSearch();
-            }
-        });
-        /*Set the animation of the time icon on the specific hour and day
-        o_date = new Date();
-        switch(o_date.getDay()){
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-            var o_hour = o_date.getHours(), o_min = o_date.getMinutes()
-            ,morning = 13, afternoon = 15, evening = 19;
-            if ((9 <= o_hour  && o_hour < morning) || (o_hour === 13 && o_min >= 0 && o_min <= 30)) {
-                crossClassList(fa_cog_icon[0]).add("fa-spin");
-            } else if((afternoon < o_hour && o_hour < evening) || 
-            (o_hour === afternoon && o_min >= 30 && o_min < 60) || 
-            (o_hour === evening && o_min >=0 && o_min <= 30)) {
-                crossClassList(fa_cog_icon[1]).add("fa-spin");
-            }
-            break;
-        case 6:
-            var o_hour = o_date.getHours(),morning = 13;
-            if (9 <= o_hour  && o_hour < morning) {
-                crossClassList(fa_cog_icon[2]).add("fa-spin");
-            }
-            break;
-        default:
-        break;
-        }*/
+        aEventListener(bus,'mouseover',animateOnHover);
+        aEventListener(bus,'mouseout',animateOnHover);
+        aEventListener(car,'mouseover',animateOnHover);
+        aEventListener(car,'mouseout',animateOnHover);
         //
         //
         //Raise nav bar if the page is scrolled
@@ -340,10 +251,9 @@
         makeFullScreen.init();
         //
         // Clear unuseful Dom Object
-        fa_cog_icon = null;
         sch = null;
-        pan1=null;
-        pan2=null;
+        bus = null;
+        car = null;
         obj.PaperMaker('button',{ element: cssSelectorAll(".paper-fab")[1]});
         obj.PaperMaker('button',{ element: cssSelectorAll(".paper-fab")[0]});
         obj.PaperMaker.MakePaperLive();
