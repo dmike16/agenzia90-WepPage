f311b91d64f6ffe35f493cb4edcbcffadd9f5dd9
dmike16 <cipmiky@gmail.com>
2015-08-26 16:49:36 +0200

    Add the '$' object to scrollingArea and resizeArea + fix the behavoir of the onscroll function so that when click to the link nav and go down for the first time the 'catalog' and 'location' sectio will be activate

--- WebComponent/www/public_html/script/interaction.js
+++ WebComponent/www/public_html/script/interaction.js
@@ -4,12 +4,13 @@
         "use strict";
 
     function onscroll_() {
-    this.previewScroll = this.lastScroll;
-        this.lastScroll = window.scrollY || window.pageYOffset;
-        this._requestTick();
+     this.previewScroll = this.lastScroll;
+     this.lastScroll = window.scrollY || window.pageYOffset;
+     this._requestTick();
     }
     function onresize_(){
-       	this.domElement.innerHeight = obj.global.innerHeight;
+       	this.px.innerHeight = obj.global.innerHeight;
+	this.px.calculate();
         this._requestTick();
     }
     function requestTick_() {
@@ -52,133 +53,165 @@
    ,cssSelectorAll = obj.doc.querySelectorAll.bind(obj.doc)
    ,crossClassList = obj.ClassList
    ,aEventListener = obj.EventUtility.aboutHandler.addListener
-   ,rEventListener = obj.EventUtility.aboutHandler.removeListener
+   ,rEventListener = obj.EventUtility.aboutHandler.removeListener;
    //
    //
    //
    // Dom Element
-   ,sch = cssSelector(".header .search")
+   var sch = cssSelector(".header .search")
    ,mask_modal = cssSelector("div.mask-modal")
    ,jb = cssSelector("button.paper-fab")
    ,arrowHint = cssSelector(".section-intro .arrow-hint")
-   ,headLogo = cssSelector(".header .logo")
    ,photoCrop = cssSelector(".section-base .photo-crop")
    ,bus = cssSelector("#location .bus p")
    ,car = cssSelector("#location .car p")
    ,middleTarget = cssSelector("#location .middle-target")
    ,fab_one = cssSelectorAll(".paper-fab")[1]
    ,photoSphere = cssSelector("#PhotoSphere")
-   ,picFrame = cssSelector(".picture-frame")
+   ,picFrame = cssSelector(".picture-frame");
    //Variables
-   ,first_click = false
+   //
+   var first_click = false;
    //
    //Object For Scrolling Event
    //
    //
-   ,scrollingPaper = {
-       view: obj.global,
-       body: obj.doc.body,
+   var scrollingArea = {
+       $:{
+	 view: obj.global,
+	 body: obj.doc.body,
+	 sectionGridOuter: cssSelector(".section-intro .primary-block")
+       },
        lastScroll: 0,
        previewScroll: 0,
        _allActive: false,
        firstScroll: false,
        ticking: false,
-       update: function () {
-           var lscroll = this.lastScroll, ihh = this.pageToScroll.fromObj.innerHeight;
-           if (lscroll < ihh || !this.firstScroll) {
-               if (lscroll > 0 && !this.firstScroll) {
-                   this.firstScroll = true;
-                   this.onOffPaper();
-               } else if (lscroll === 0 && this.firstScroll) {
-                   this.firstScroll = false;
-                   this.onOffPaper();
-               }
-               
-               var ptsfo = this.pageToScroll.fromObj;
-               var section = ptsfo.sections[0], content = this.pageToScroll.contentOpacity;
-               var thh = ihh/3, nhh= ihh*0.8;
-	       
-               if (lscroll >= thh && !this.tirth) {
-                   var qhh = -ihh/5;
-                   section.style.transform="translate3d(0px," + qhh +"px,0px)";
-                   section.style.zIndex = "-1";
-                   content.style.opacity="0.01";
-                   this.tirth = true;
-               } else if (lscroll < thh && this.tirth) {
-                   section.style.transform="translate3d(0px, 0px,0px)";
-                   section.style.zIndex="0";
-                   content.style.opacity="1.0";
-                   this.tirth = false;
-               }
-	       
-               if (photoCrop !== null) {
-                   if (lscroll >= nhh){
-                       this.body.dispatchEvent(new Event('scrolledIntroPage'));
-                   }
-               }
-           } else if (!this._allActive) {
-               var nih,lim;
-               if(!this._catalogActive){
-                   nih = 2*ihh;
-                   lim = nih*0.8;
-                   if (lscroll > lim) {
-                       this._catalogActive = true;
-                       _setActive(this.catalogServices);
-                       this.catalogServices = null;
-                   }
-               } else if (!this._locationActive){
-                   nih = 3*ihh;
-                   lim = nih*0.8;
-                   if (lscroll > lim) {
-                       this._locationActive = true;
-                       this._allActive = true;
-                       _setActive(this.location);
-                       this.location = null;
-                   }
-               }
-           }
-       },
-       onOffPaper: function () {
-           var fs = this.firstScroll;
-           crossClassList(this.body).toggle("scrolling",fs);
-           crossClassList(jb).toggle("active",fs);
-           crossClassList(arrowHint).toggle("deactive",fs);
-       },
        tirth: false,
+       update: function () {
+	var lscroll = this.lastScroll
+	,ihh = this.px.innerHeight;
+	if (lscroll < ihh || !this.firstScroll) {
+         if (lscroll > 0 && !this.firstScroll) {
+          this.firstScroll = true;
+          this.handleDynamicElement();
+	  this.activeElementWhenScrolledToLim("catalogServices",this.px.pages[2],_setActive);
+	  this.activeElementWhenScrolledToLim("location",this.px.pages[3],_setActive);
+	  
+         } else if (lscroll === 0 && this.firstScroll) {
+          this.firstScroll = false;
+          this.handleDynamicElement();
+         }
+         
+         var section = this.$.sections[0]
+	 ,content = this.$.sectionGridOuter;
+	 
+         if (lscroll >= this.px.pages[0] && !this.tirth) {
+          var qhh = -ihh/5;
+          section.style.transform="translate3d(0px," + qhh +"px,0px)";
+          section.style.zIndex = "-1";
+          content.style.opacity="0.01";
+          this.tirth = true;
+         } else if (lscroll < this.px.pages[0] && this.tirth) {
+          section.style.transform="translate3d(0px, 0px,0px)";
+          section.style.zIndex="0";
+          content.style.opacity="1.0";
+          this.tirth = false;
+         }
+	       
+         if (photoCrop !== null) {
+          if (lscroll >= this.px.pages[1]){
+           this.$.body.dispatchEvent(new Event('scrolledIntroPage'));
+          }
+         }
+          } else if (!this._allActive) {
+           if(!this.$._cacheStatusEle['catalogServices']){
+             this.activeElementWhenScrolledToLim("catalogServices",this.px.pages[2],_setActive);
+            
+           } else if (!this.$._cacheStatusEle['location']){
+             this.activeElementWhenScrolledToLim("location",this.px.pages[3],_setActive);
+            }
+          }
+       },
+       handleDynamicElement: function () {
+        var fs = this.firstScroll;
+        crossClassList(this.$.body).toggle("scrolling",fs);
+        crossClassList(jb).toggle("active",fs);
+        crossClassList(arrowHint).toggle("deactive",fs);
+       },
+       activeElementWhenScrolledToLim: function (stringEle, lim, callback) {
+	 if (this.lastScroll > lim) {
+	   var eleDom = this.$[stringEle];
+	  
+	   callback(eleDom);
+	   var status = this.$._cacheStatusEle, p = "";
+	   
+	   status[stringEle] = true;
+	  
+	   for (p in status){
+	    if (status.hasOwnProperty(p)){
+	     if (!status[p]){
+	      break;
+	      }
+	     }
+	    }
+	  this._allActive = status[p];
+	   
+	  }
+       },
        init: function () {
-           this._update = update_.bind(this);
-           this._requestTick = requestTick_.bind(this);
-           this.scroll = onscroll_.bind(this);
-           this.scroll();
+        this._update = update_.bind(this);
+        this._requestTick = requestTick_.bind(this);
+        this.scroll = onscroll_.bind(this);
+        this.scroll();
            
-           aEventListener(this.view, "scroll", this.scroll,false);
-           delete(this.init);
+        aEventListener(this.$.view, "scroll", this.scroll,false);
+        delete(this.init);
        }
-   }
+   };
    //Object For Resize Event
    //
    //
-   ,makeFullScreen = {
-       domElement : {
+   var resizeArea = {
+       $ : {
            sections: cssSelectorAll("section"),
-           innerHeight: obj.global.innerHeight,
            body: obj.doc.body
+       },
+       px :{
+	 innerHeight: obj.global.innerHeight,
+	 pages: [],
+	 calculate: function pxCalculate(){
+	  /*
+	   * The first page it's always in full screen
+	   */
+	  this.pages[0] = this.innerHeight/3;
+	  
+	  var portion = 0.8;
+	  this.pages[1] = this.innerHeight * portion;
+	  
+	  /*
+	   * The Other pages have a min-heigh value of 610px;
+	   */
+	   var currentHeight =  (this.innerHeight < 610)? 610: this.innerHeight;
+	   this.pages[2] = (this.innerHeight + currentHeight)*portion;
+	   this.pages[3] = (this.innerHeight + 2*currentHeight)*portion;
+	  
+	  }
        },
        ticking: false,
        update: function () {
-           var domEle = this.domElement
-           ,hh = domEle.innerHeight
-           ,shh = hh + "px"
-           ,sections = domEle.sections
+           var height = this.px.innerHeight
+           ,sheight = height + "px"
+           ,sections = this.$.sections
            ,fsection = sections[0];
-           domEle.body.style.paddingTop = shh;
+           this.$.body.style.paddingTop = sheight;
            for (var i = 0, len = sections.length; i < len; i++) {
-               sections[i].style.height = shh;
+               sections[i].style.height = sheight;
            }
-           if (hh <= 350) {
-               var qhh = -hh/4;
-               fsection.style.webkitTransform = "translate3d(0px,"+ qhh +"px,0px)";
-               fsection.style.transform = "translate3d(0px,"+ qhh +"px,0px)";
+           if (height <= 350) {
+               var qheight = -height/4;
+               fsection.style.webkitTransform = "translate3d(0px,"+ qheight +"px,0px)";
+               fsection.style.transform = "translate3d(0px,"+ qheight +"px,0px)";
            } else {
                fsection.style.webkitTransform = null;
                fsection.style.transform = null;
@@ -194,20 +227,17 @@
            delete(this.init);
        }
    };
-			       
-   scrollingPaper.pageToScroll = { 
-       fromObj : makeFullScreen.domElement,
-       contentOpacity : cssSelector(".section-intro .primary-block")
+   scrollingArea = obj.Class.extendByCopy(resizeArea,scrollingArea,['$','px']);
+   scrollingArea.$.catalogServices = cssSelectorAll("#catalog .service-title-0");
+   scrollingArea.$.location = cssSelectorAll("#location .map");
+   scrollingArea.$._cacheStatusEle = {
+     'catalogServices': false,
+    'location' : false
    };
-   scrollingPaper.catalogServices = cssSelectorAll("#catalog .service-title-0");
-   scrollingPaper._catalogActive = false;
-   scrollingPaper.location = cssSelectorAll("#location .map");
-   scrollingPaper._locationActive = false;
    //
    // Event Callback function
    //
    function gestureEvent(evt) {
-       
        switch (evt.type) {
        case 'focus':
            crossClassList(evt.currentTarget).add("active");
@@ -266,8 +296,8 @@
     //
     //
     //Raise nav bar if the page is scrolled
-    scrollingPaper.init();            
-    makeFullScreen.init();
+    scrollingArea.init();            
+    resizeArea.init();
     //
     // Clear unuseful Dom Object
     sch = null;
@@ -276,7 +306,6 @@
     obj.PaperMaker('button',{ element: fab_one});
     obj.PaperMaker('button',{ element: cssSelectorAll(".paper-fab")[0]});
     obj.PaperMaker.MakePaperLive();
-    console && console.log("%c90 s r l s\n%cPratiche Auto\nTel 06 01905227","font-size:1.5em;color:#1945D5;", "color:#14BD4C;font-size:1em;");
-        });
+    });
 }());
 
