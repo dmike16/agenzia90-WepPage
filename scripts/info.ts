21f7e3bbd6e52e3ccb837a8865c02881c53a9975
dmike16 <cipmiky@gmail.com>
2019-01-03 23:15:35 +0100

    build: 3.5/7 add info script task

--- scripts/info.ts
+++ scripts/info.ts
@@ -1,7 +1,10 @@
 import { Logger } from "../tools/logger";
 
 export default async function(args: any, root: string, log: Logger){
-    log.info('studio90srls project building info');
+    log.info('********** studio90srls ***********');
+    log.info('https://studio90srls.it');
+    log.info('Via Silvestro Gherardi 24');
+    log.info('***********************************');
 
     return 0;
 }
