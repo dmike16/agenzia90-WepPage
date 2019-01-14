fd633ba47218003a60a12f0bf9a450143182330d
dmike16 <cipmiky@gmail.com>
2019-01-14 22:43:07 +0100

    build: 2/3 create build webpack process. using Webpack Node API.

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,15 @@
 import { Logger } from "../tools/logger";
+import { WebpackCompilationOutput, webpackBundle } from "../tools/webpack/compiler";
 
 export default async function(args: any, log: Logger) {
-    log.info("FIXME(dmike16): create webpack configuratiion")
+    log.info("FIXME(dmike16): create webpack configuratiion");
+    try {
+        const bundle: WebpackCompilationOutput = await webpackBundle({});
+        log.info(bundle.result);
+    }catch(err) {
+        log.error(err.error);
+        return 1;
+    }
+
+    return 0;
 }
